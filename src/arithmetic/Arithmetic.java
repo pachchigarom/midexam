6db0d4d1e3b23d8e8d3e5691b7b244739c8f7edf
ompac <ompac@10.48.10.209>
2023-07-10 14:20:14 -0400

    Version 1

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -3,34 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arithmetic;
 
 
-import java.util.Scanner;
-import static java.time.Clock.system;
+public class Arithmetic {
+    public static void main(String[] args) {
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
 
-/** This class calls the method to perform 
- * arithmetic operations based on user input
- * execute the code check the output
- * @author sivagamasrinivasan
- * 
- */
-public class Arithmetic 
-{
+        System.out.println("Enter arithmetic operation to perform: ");
+        String operationStr = in.next();
+        Operation operation = Operation.valueOf(operationStr.toUpperCase());
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+        System.out.println("Enter two numbers: ");
+        double n = in.nextDouble();
+        double m = in.nextDouble();
+
+        double result = r.calculate(n, m, operation);
+        System.out.println("Result: " + result);
     }
 }
-
